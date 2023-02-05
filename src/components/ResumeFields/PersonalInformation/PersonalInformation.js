ff519daf6a23dc70832a19b7614abd985f622432
ia popkhadze <iafofkhadze@gmail.com>
2023-02-05 02:21:26 -0800

    have done styling personal information page

--- src/components/ResumeFields/PersonalInformation/PersonalInformation.js
+++ src/components/ResumeFields/PersonalInformation/PersonalInformation.js
@@ -1,17 +1,96 @@
-import React from 'react'
-import {useFormik} from 'formik'
+import React from "react";
+import { useFormik } from "formik";
+import "./PersonalInformationStyle/personalInformationStyle.css";
 const PersonalInformation = () => {
-    const formik=useFormik({
-        initialValues:{
-
-        }
-    })
+  const formik = useFormik({
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      image: "",
+      about: "",
+      email: "",
+      mobile: "",
+    },
+  });
+  console.log(formik)
 
   return (
-    <form autoComplete='off'>
-       
-    </form>
-  )
-}
+    <>
+      <div className="personal_information">
+        <p className="title">პირადი ინფო</p>
+        <div className="current_page_index" style={{ fontSize: "20px" }}>
+          1/3
+        </div>
+      </div>
+      <form action="">
+        <div className="firstname_lastname_container ">
+          <div>
+            <label htmlFor="firstname">სახელი</label>
+            <input
+              value={formik.values.firstname}
+              onChange={formik.handleChange}
+              name="firstname"
+              type="text"
+              placeholder="სახელი..."
+            />
+            <p>მინიმუმ ორი ასო, ქართული ასოები</p>
+          </div>
+          <div>
+            <label htmlFor="lastname">გვარი</label>
+            <input
+              value={formik.values.lastname}
+              onChange={formik.handleChange}
+              name="lastname"
+              type="text"
+              placeholder="გვარი..."
+            />
+            <p>მინიმუმ ორი ასო, ქართული ასოები</p>
+          </div>
+        </div>
+        <div className="upload_image_container">
+          <p>პირადი ფოტოს ატვირთვა</p>
+          <button>ატვირთვა</button>
+        </div>
+        <div className="about_me_container">
+          <label>ჩემ შესახებ (არასავალდებულო)</label>
+          <textarea
+            value={formik.values.about}
+            onChange={formik.handleChange}
+            name="about"
+            placeholder="ზოგადი ინფო შენს შესახებ"
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+        <div className="input_container">
+          <label htmlFor="email">ელ.ფოსტა</label>
+          <input
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            name="email"
+            type="email"
+            placeholder="someone@redberry.ge"
+          />
+          <p>უნდა მთავრდებოდეს @redberry.ge-ით</p>
+        </div>
+        <div className="input_container">
+          <label htmlFor="email">მობილურის ნომერი</label>
+          <input
+            value={formik.values.mobile}
+            onChange={formik.handleChange}
+            name="mobile"
+            type="text"
+            placeholder="+995 _ _  _ _  _ _"
+          />
+          <p>უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს</p>
+        </div>
+        <div className="btn_container">
+          <button className="next_page_btn">შემდეგი</button>
+        </div>
+      </form>
+    </>
+  );
+};
 
-export default PersonalInformation
+export default PersonalInformation;
--- src/components/ResumeFields/ResumeFields.js
+++ src/components/ResumeFields/ResumeFields.js
@@ -1,41 +1,43 @@
 import React from "react";
 import PersonalInformation from "./PersonalInformation/PersonalInformation";
+import Experience from "./Experience/Experience.js";
+import Education from "./Education/Education.js";
 import { IoIosArrowBack } from "react-icons/io";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 const ResumeFields = () => {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
+  const pages = [<PersonalInformation />, <Experience />, <Education />];
   return (
-    <div >
-      <div style={myStyle.backToStartPage} onClick={()=>navigate('/')}>
+    <div>
+      <div style={myStyle.backToStartPage} onClick={() => navigate("/")}>
         <IoIosArrowBack />
       </div>
-      <div style={myStyle.currentField}>
-        <PersonalInformation />
-      </div>
+      <div style={myStyle.currentField}>{pages[0]}</div>
     </div>
   );
 };
 
 const myStyle = {
   backToStartPage: {
-    position:'absolute',
-    top:'45px',
-    left:'48px',
+    position: "absolute",
+    top: "45px",
+    left: "48px",
     width: "40px",
     height: "40px",
     borderRadius: "50%",
     backgroundColor: "white",
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    fontSize:'1.3rem',
-    cursor:'pointer'
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "1.3rem",
+    cursor: "pointer",
   },
-  currentField:{
-    position:'absolute',
-    top:'47px',
-    left:'149px'
-  }
+  currentField: {
+    position: "absolute",
+    top: "47px",
+    left: "149px",
+    width: "798px",
+  },
 };
 
 export default ResumeFields;
